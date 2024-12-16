21de9951694033c5d89c462bc7bcb7cc5818da2e
Charlotte Gracia <56256920+charlottegracia@users.noreply.github.com>
2024-12-16 13:26:49 +0100

    test

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return [
+        'Laravel' => app()->version(),
+        'Message' => 'test'
+    ];
 });
 
 require __DIR__.'/auth.php';

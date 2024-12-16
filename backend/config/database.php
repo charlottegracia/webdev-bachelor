d8f15dde7e39557662ff3cf8763b9f0360420872
Charlotte Gracia <56256920+charlottegracia@users.noreply.github.com>
2024-12-16 14:49:15 +0100

    test

--- backend/config/database.php
+++ backend/config/database.php
@@ -45,7 +45,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'mysql.railway.internal'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3307'),
             'database' => env('DB_DATABASE', 'webdev-bachelor'),
             'username' => env('DB_USERNAME', 'webdev-bachelor'),

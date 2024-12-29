90fb0ba98fd5c5790a2dd5007d5e60041be81967
Charlotte Gracia <56256920+charlottegracia@users.noreply.github.com>
2024-12-29 15:45:20 +0100

    input component #13

---
+++ frontend/components/InputField.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex flex-col">
+        <label v-if="label" class="text-lg mb-2">{{ label }}</label>
+
+        <input :type="type" :placeholder="placeholder" :class="[
+            'bg-[#F8F8F8] border-[#EBEBEB] text-[#9EA4AF] rounded-xl px-4 py-3',
+            { 'w-fit': type === 'date' }
+        ]" :aria-label="label" />
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    label: {
+        type: String,
+        required: false,
+        default: '',
+    },
+    placeholder: {
+        type: String,
+        required: false,
+        default: '',
+    },
+    type: {
+        type: String,
+        required: false,
+        default: 'text',
+    },
+});
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -9,6 +9,9 @@
     <div class="text-9xl text-text-default">forside</div>
     <div class="text-9xl text-text-default">forside</div>
     <div class="text-9xl text-text-default">forside</div>
+    <InputField label="Brugernavn" placeholder="Indtast dit brugernavn" type="text" />
+    <InputField label="Adgangskode" placeholder="Indtast din adgangskode" type="password" />
+    <InputField label="Alder" placeholder="Indtast din alder" type="date" />
     <Banner>
         <div class="fields text-[64px] text-homeblue-100">Hold dig opdateret</div>
     </Banner>

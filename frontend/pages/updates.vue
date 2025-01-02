90dc0a803e42a821ee34ea20597d724b8a213c89
Josefine Jensen <56688510+josefine98@users.noreply.github.com>
2025-01-02 11:34:08 +0100

    #15

--- frontend/pages/updates.vue
+++ frontend/pages/updates.vue
@@ -1,3 +1,9 @@
 <template>
     <div>Liveopdateringer</div>
+    <div class="mt-40">
+        <p class="mt-10">tags</p>
+        <Tag text="Spanien" type="country"/>
+        <Tag text="DHL Express" type="carrier"/>
+        <Tag text="En IT-Service" type="itService"/>
+    </div>
 </template>

d1338076717ba2f7c8ceacbf8d5eba0d9540a75d
Josefine Jensen <56688510+josefine98@users.noreply.github.com>
2025-01-10 11:29:59 +0100

    trying to fix time problems

--- frontend/test/setup.ts
+++ frontend/test/setup.ts
@@ -5,4 +5,9 @@ vi.mock('#app', () => ({
       apiBase: 'http://localhost:3000',
     })),
   }));
-  
+
+
+// Mock the Date object to a fixed date and time
+const fixedDate = new Date('2023-10-01T14:00:00Z');
+vi.useFakeTimers();
+vi.setSystemTime(fixedDate);
